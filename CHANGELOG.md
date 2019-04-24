3a8a5b5479a7ba45fb963b91db796292c4902d2c
hieunba <hi3unb@gmail.com>
2019-04-24 22:29:26 +0700

    Add generated cookbook content

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# dbeaver CHANGELOG
+
+This file is used to list changes made in each version of the dbeaver cookbook.
+
+# 0.1.0
+
+Initial release.
+
+- change 0
+- change 1
+
---
+++ metadata.rb
@@ -0,0 +1,20 @@
+name 'dbeaver'
+maintainer 'Nghiem Ba Hieu'
+maintainer_email 'hi3unb@gmail.com'
+license 'Apache-2.0'
+description 'Installs/Configures dbeaver'
+long_description 'Installs/Configures dbeaver'
+version '0.1.0'
+chef_version '>= 13.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/dbeaver/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/dbeaver'
